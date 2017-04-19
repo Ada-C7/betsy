328039aab7732f7d7281365b1f379a5693c0b1a0
amb54 <ambarklund@hotmail.com>
2017-04-19 14:32:48 -0700

    Added fields to /migrate/ _create_products.rb

--- db/migrate/20170419192340_create_products.rb
+++ db/migrate/20170419192340_create_products.rb
@@ -1,6 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
+      t.belongs_to :merchant
+      t.string :category
+      t.string :name
+      t.number :price
+      t.string :description
+      t.string :image
+      
+      t.integer :inventory
+      t.string :status
 
       t.timestamps
     end
