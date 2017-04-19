997ea06df7d7969f01a00663f2451ed8c1c03c21
Spatterjaaay <aja.pursova@gmail.com>
2017-04-19 15:56:25 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "producs#homepage"
+
+  get "/auth/github/callback", to: "sessions#create"
+  delete "/logout", to: "sessions#logout"
+
+  resources :categories do
+    get "/products", to: "products#index"
+  end
+
+  resources :products
+  resources :users
+  resources :orders
+
+
 end
