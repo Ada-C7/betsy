bff036c4413ec7c78b8eb1bf9caf388711ff9e9a
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-19 16:21:59 -0700

    merchant controller methods

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,38 @@
 class MerchantsController < ApplicationController
+  def index
+    @merchants = Merchant.all
+  end
+
+  def show
+    @merchant = Merchant.find_by(params: id)
+  end
+
+  def login_form;  end
+
+  def login
+    merchant = Merchant.find_by(username: params[:username])
+    if user
+      session[:merchant_id] = merchant.id
+      flash[:success] = "Successfully logged in as existing user #{merchant.username}"
+      redirect_to root_path
+    elsif
+      merchant = Merchant.create(username: params[:username])
+      session[:merchant_id] = merchant.id
+      flash[:success] = "Successfully created new user #{merchant.username} with ID #{merchant.id}"
+      redirect_to root_path
+    else
+      if user.errors.any?
+        user.errors.each do |column, message|
+          flash.now[:error] = "Could not log in #{message}"
+        end
+        render :login_form
+      end
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out."
+    redirect_to root_path
+  end
 end
