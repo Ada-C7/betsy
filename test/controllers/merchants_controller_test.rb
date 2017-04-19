d8da0393361e6196c7c72fbc6ada029c36aa8868
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-19 16:30:46 -0700

    wrote merchant show tests

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -2,7 +2,23 @@ require "test_helper"
 
 describe MerchantsController do
 
+
+
   describe 'show' do
-    
+    let (:merchant) { Merchant.first}
+
+    it 'shows a merchants page' do
+      get merchant_path(merchant.id)
+      must_respond_with :success
+    end
+
+    it 'returns a 404 if merchant DNE' do
+      merchant2 = Merchant.last
+      merchant2.destroy
+      get merchant_path(merchant2.id)
+      must_respond_with :not_found
+    end
+
+    end
   end
 end
