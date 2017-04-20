aaf735b5b66308729cf86a4ddbb836202d6b5e9a
Brenna <bcmdarroch@gmail.com>
2017-04-20 10:40:00 -0700

    wrote some failing fixtures

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,11 +1,64 @@
 require "test_helper"
 
 describe Product do
-  let(:product) { Product.new }
-
-# must be valid
+  let(:product) { products(:kidjams, :famjams, :jamjams, :hotdamnjams).sample }
 
 # 2+ tests for each validation on a model
+describe "validations" do
+  it "is valid with a unique name" do
+    # product
+  end
+
+  it "is invalid without a name" do
+    # products(:noname)
+  end
+
+  it "is invalid without a unique name" do
+    # product(:samename)
+  end
+
+  it "is valid with a quantity" do
+    # product
+  end
+
+  it "is invalid without a quantity" do
+    # products(:noquantity)
+  end
+
+  it "is valid with a price" do
+    # product
+  end
+
+  it "is invalid without a price" do
+    # products(:noprice)
+  end
+
+  it "is valid with a description" do
+    # product
+  end
+
+  it "is invalid without a description" do
+    # products(:nodesc)
+  end
+
+  it "is valid with a image" do
+    # product
+  end
+
+  it "is invalid without a image" do
+    # products(:noimg)
+  end
+
+  it "is valid with a user" do
+    # product
+  end
+
+  it "is invalid without a user" do
+    # products(:nouser)
+  end
+
+end
+
 
 # 1+ test for each model relationship
 
