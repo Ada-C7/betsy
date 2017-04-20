e32a9f364e58bc2034e10dcbd69d4bb7e7ef20e8
Spatterjaaay <aja.pursova@gmail.com>
2017-04-20 14:44:06 -0700

    added temporary user fixture, fixed seed code, added validations for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ApplicationRecord
   has_many :order_items
   belongs_to :user, optional: true
+
+  validates :status, presence: true
+  validates :email, presence: true, format: {with: /@/}
+  validates :billing_name, presence: true
+  validates :address, presence: true
+  validates :card_number, presence: true, length: { in: 14..19 }
+  validates :card_expiration, presence: true
+  validates :cvv, presence: true, length: { is: 3 }
+  validates :zipcode, presence: true
 end
