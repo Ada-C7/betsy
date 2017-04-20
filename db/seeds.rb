913c55b1d7b9554563466e7b920bb6b73933f616
Spatterjaaay <aja.pursova@gmail.com>
2017-04-20 10:10:54 -0700

    added code to import seeds from csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+orders = []
+
+CSV.foreach("orders_seeds.csv", { :headers => true }) do |line|
+  drivers << {status: line[0], email: line[1], billing_name: line[2], address: line[3], card_number: line[4], card_exporation: line[5], cvv: line[6], zipcode: line[7], user_id: line[8] }
+end
+
+success_count = 0
+
+orders.each do |order|
+  temp_order = Order.create(order)
+  if temp_order.id
+    success_count += 1
+    puts "#{temp_order.email} successfully added"
+  end
+end
+
+puts "#{success_count} out of #{orders.length} successfully added"
