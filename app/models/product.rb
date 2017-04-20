a259f518c34883c92ebe0d6c8673bcb647a5d120
amb54 <ambarklund@hotmail.com>
2017-04-19 17:15:28 -0700

    Updated fixtures for products and merchants. Started the product_test.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, uniqueness: true, numericality: { greater_than: 0 }
+  belongs_to :merchant
+  # has_many :reviews
+  # has_many :orders, through :orderproducts
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,21 @@
 require "test_helper"
 
 describe Product do
-  let(:product) { Product.new }
+  describe "relations" do
+    # it "product has a mmerchant" do
+    #    product = products(:product1)
+    #    product.must_respond_to :merchant
+    #    product.merchant.must_be_kind_of Merchant
+    # end
+  end # END of describe "relations"
 
-  it "must be valid" do
-    value(product).must_be :valid?
+  describe "validations" do
+    it "requires a name" do
+      product = Product.new(price: 7.30)
+      product.valid?.must_equal false
+      product.errors.messages.must_include :name
+    end
   end
-end
+
+
+end # END of describe Product
