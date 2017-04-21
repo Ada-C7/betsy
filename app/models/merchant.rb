4e2e5070ad5463c75d74a4b71ee53f20aeb6f47e
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-21 12:03:19 -0700

    updated merchant model for github auth

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,13 @@ class Merchant < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   has_many :products
 
+  def self.from_github(auth_hash)
+    merchant = Merchant.new
+    merchant.username = auth_hash["info"]["nickname"]
+    merchant.email = auth_hash["info"]["email"]
+    merchant.oauth_uid = auth_hash["uid"]
+    merchant.oauth_provider = "github"
+    return merchant
+  end
+
 end
