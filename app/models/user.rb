625ef249594ddec74f8dc550695d385450eb435a
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-20 21:58:58 -0700

    adds oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  def self.create_from_github(auth_hash)
+    User.create(
+      username: auth_hash["info"]["name"],
+      provider: auth_hash["provider"],
+      uid: auth_hash["uid"],
+      email: auth_hash["info"]["email"]
+    )
+  end
+
 end
