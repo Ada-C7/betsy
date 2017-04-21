f1dfbe3ce07e2a551ebc3f796d268d73dae21818
amb54 <ambarklund@hotmail.com>
2017-04-21 09:00:19 -0700

    Changed the validation for price in the product model to not be unique. Updated products view for show and index with info and dummy link buttons.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :price, presence: true, uniqueness: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
   belongs_to :merchant
   # has_many :reviews
   # has_many :orders, through :orderproducts
