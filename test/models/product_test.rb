f3cfad6fd95717e3c9c2bb632afd4044b910dc13
amb54 <ambarklund@hotmail.com>
2017-04-21 10:45:47 -0700

    Minor changes to product model tests.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,14 +3,18 @@ require "test_helper"
 describe Product do
 
   describe "relations" do
-    it "product has a mmerchant" do
+    it "product has a merchant" do
        product = products(:product1)
        puts product.name
        product.must_respond_to :merchant
        product.merchant.must_be_kind_of Merchant
     end
     #  it "product has many reviews" do
+
     #  end
+    # it "has many productorders" do
+    #
+    # end
   end # END of describe "relations"
 
   describe "validations" do
@@ -25,7 +29,6 @@ describe Product do
       puts product.name
       new_product = Product.new(name: "Food1", price: 7.30)
       new_product.valid?.must_equal false
-
     end
 
     it "requires a price" do
