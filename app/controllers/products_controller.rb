4c8108ea040044293d3f8d618724cf44700a9c20
amb54 <ambarklund@hotmail.com>
2017-04-21 12:55:42 -0700

    Added product views new and _form. Added code to both files. Added actions new and create to the product controller.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,27 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Work.new(media_params)
+    # @product.merchant_id = session[:merchant_id]
+    @product.merchant_id = 1 #Just for tests
+    if @product.save
+      # flash[:status] = :success
+      # flash[:result_text] = "Successfully created #{@product.id}"
+      redirect_to
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not create #{@product.id}"
+      flash[:messages] = @product.errors.messages
+      render :new, status: :bad_request
+    end
+
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
   end
