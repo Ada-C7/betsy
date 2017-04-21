e7860a33faae88bebba03680a54e79a33b541c92
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-20 21:13:32 -0700

    adds basic model tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,31 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  it "doesn't allow a user without a username" do
+    user = User.new
+    user.valid?.must_equal false
+    user.errors.messages.must_include :username
   end
+
+  it "doesn't allow two users with the same username" do
+    one = users(:one)
+    duplicate = User.new(username: one.username, email: "sandwich@gmail.com")
+
+    duplicate.valid?.must_equal false
+  end
+
+  it "doesn't allow a user without an email" do
+    user = User.new
+    user.valid?.must_equal false
+    user.errors.messages.must_include :username
+  end
+
+  it "doesn't allow two users with the same email" do
+    one = users(:one)
+    duplicate = User.new(username: "Sandwich", email: one.email)
+
+    duplicate.valid?.must_equal false
+  end
+
 end
