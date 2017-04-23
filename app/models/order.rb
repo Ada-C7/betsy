2d9e29a9cc4acad80c0bd6dbb6c51c49f4d770d1
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-22 21:35:19 -0700

    working on validations for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   has_many :productorders
   # has_many :products, through: :productorders
   has_and_belongs_to_many :products
+
+  validates :customer_name, presence: true
+  validates :customer_address, presence: true
+  validates :customer_email, presence: true
+  validates :customer_cc_info, presence: true,
+                               numericality: { only_integer: true },
+                               length: { is: 16 }
+  validates :status, inclusion: { in: %w(paid) }
 end
