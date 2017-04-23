092b197e8cbd49df51092ef532893ac2d3ac7657
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-22 21:07:57 -0700

    refracted routes and order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,17 +4,20 @@ class OrdersController < ApplicationController
 
   def cart
     @products = Productorder.where(orders_id: session[:order_id])
-    # raise
   end
 
   def add_item
     order = current_order
-    product_order = Productorder.new
-    product_order.products_id = params[:product_id]
-    product_order.orders_id = session[:order_id]
-    product_order.quantity = 1
-    # raise
-    product_order.save
-    redirect_to cart_path
+    product_order = Productorder.add_product(params[:product_id], session[:order_id] )
+    if product_order.valid?
+      product_order.save
+      redirect_to cart_path
+    else
+      flash.now[:failure] = "Could not add item"
+      render :back
+    end
   end
+
+  # def complete_order
+  # end
 end
--- app/models/productorder.rb
+++ app/models/productorder.rb
@@ -1,4 +1,12 @@
 class Productorder < ApplicationRecord
   belongs_to :product
   belongs_to :order
+
+  def self.add_product(product_id, order_id)
+    product_order = Productorder.new
+    product_order.products_id = product_id
+    product_order.orders_id = order_id
+    product_order.quantity = 1
+    return product_order
+  end
 end
