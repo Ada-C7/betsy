e2cb9d9685506f487c5b18d5ae8f171bc22fa9c9
amb54 <ambarklund@hotmail.com>
2017-04-22 18:00:02 -0700

    In the product model: Uncommented the relationship has_many :reviews. Added a method for calculating the average ratingof the product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,21 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   belongs_to :merchant
-  # has_many :reviews
+  has_many :reviews
   has_many :productorders
   has_many :orders, through: :productorders
 
-end
+
+
+  def average_rating
+    array_of_ratings = reviews.all.collect {|t| t.rating}.select(&:present?)
+    if array_of_ratings != []
+      avg = array_of_ratings.sum.to_f/array_of_ratings.length
+      return avg.round(1)
+    else
+      return "-"
+    end
+  end
+
+
+end # END of class Product
