ed6c5128a0b11b45fb96446c48baf978485343f6
amb54 <ambarklund@hotmail.com>
2017-04-22 19:11:31 -0700

    Added a private section with the methods product_params and find_user.Hard coded value for the instance variable @login_user. Added filter :find_user. Removed @login_user from the show action.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-
+  before_action :find_user
 
   def root
     @products = Product.all
@@ -14,25 +14,38 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Work.new(media_params)
-    # @product.merchant_id = session[:merchant_id]
-    @product.merchant_id = 1 #Just for tests
+    @product = Product.new(product_params)
+    @product.merchant_id = @login_user.id
     if @product.save
       # flash[:status] = :success
       # flash[:result_text] = "Successfully created #{@product.id}"
-      redirect_to
+      redirect_to products_path
     else
-      flash[:status] = :failure
-      flash[:result_text] = "Could not create #{@product.id}"
-      flash[:messages] = @product.errors.messages
-      render :new, status: :bad_request
+      # flash[:status] = :failure
+      # flash[:result_text] = "Could not create #{@product.id}"
+      # flash[:messages] = @product.errors.messages
+      render :new #, status: :bad_request
     end
 
   end
 
   def show
     @product = Product.find_by(id: params[:id])
-    @login_user = Product.find_by(id: 1).merchant # Must be removed when right code for @login_user is added to the ApplicationController
+    # @login_user = Product.find_by(id: 1).merchant # Must be removed when right code for @login_user is added to the ApplicationController
   end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :image, :category, :status, :description)
+  end
+
+  def find_user
+    @login_user = Product.find_by(id: 1).merchant
+    # if session[:user_id]
+    #   @login_user = User.find_by(id: session[:user_id])
+    # end
+  end
+
+
 
 end # END of class ProductsController
