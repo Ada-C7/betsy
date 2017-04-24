65916c032fe2994d9e064806aa5d0bc10170f453
Brenna <bcmdarroch@gmail.com>
2017-04-24 11:32:24 -0700

    fixed product conflict

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,10 +5,7 @@ class Product < ApplicationRecord
   has_and_belongs_to_many :categories
 
   validates :name, presence: true, uniqueness: true
-<<<<<<< HEAD
-=======
   validates :price, presence: true, numericality: { greater_than: 0 }
->>>>>>> master
   validates :quantity, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, presence: true
