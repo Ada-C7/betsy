869a34526241e0e877d9165a34b0b9cc2f85e0ce
Spatterjaaay <aja.pursova@gmail.com>
2017-04-24 14:18:13 -0700

    created routes for cart functionality and added link to add to cart in product show page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,10 @@ Rails.application.routes.draw do
 
   resources :products, except: :index
   resources :users
-  resources :orders
+  resources :orders, except: [:index, :create, :new, :destroy]
+  post "/orders/:id/add", to: "orders#add", as: "add_item"
+  post "/orders/:id/remove", to: "orders#remove", as: "remove_item"
+  get "/carts", to: "orders#index"
 
   get "/account", to: "users#account"
 end
