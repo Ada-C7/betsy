7623d43f896cd6f061b4c5393b1921a4d6331f89
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-04-24 12:34:47 -0700

    log in view and logged out view for merchant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :find_merchant
+
+  def find_merchant
+    if session[:merchant_id]
+      @current_user = Merchant.find_by(id: session[:merchant_id])
+    end
+  end
 end
