5f36cac28a931efc33cfa4cc40089c11433ea8fd
Brenna <bcmdarroch@gmail.com>
2017-04-24 11:27:04 -0700

    cleaned up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,19 +43,3 @@ orders.each do |order|
 end
 
 puts "#{success_count} out of #{orders.length} successfully added"
-
-# require 'csv'
-#
-# products_array = []
-# CSV.read("db/products.csv", headers: true).map do |line|
-#   product = {user_id: line[1], name: line[2], quantity: line[3], price: line[4], description: line[5], image_url: line[6]}
-#   products_array << product
-# end
-#
-# products_array.each do |product|
-#   new_product = Product.create!(product)
-#   if !new_product.id
-#     puts "couldn't create product #{product.name}"
-#   end
-# end
-#
