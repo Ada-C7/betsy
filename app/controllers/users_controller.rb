69cb2cc5b509a569143baa26d38cceb245349dd8
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-24 14:22:54 -0700

    adds account show and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,18 @@ class UsersController < ApplicationController
 
   def show; end
 
-  def update; end
+  def update
+    if @logged_in_user.update(user_params)
+      flash[:status] = :success
+      flash[:result_text] = "Successfully updated profile"
+      redirect_to account_path
+    else
+      flash.now[:status] = :failure
+      flash.now[:result_text] = "Could not update profile"
+      flash.now[:messages] = @logged_in_user.errors.messages
+      render :edit, status: :bad_request
+    end
+  end
 
   def create; end
 
@@ -18,4 +29,10 @@ class UsersController < ApplicationController
   def products; end
 
   def orders; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :image_url, :provider, :uid)
+  end
 end
