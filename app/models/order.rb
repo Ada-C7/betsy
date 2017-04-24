9c019764ff67d3ed6e9a652a3392a757a960a985
Spatterjaaay <aja.pursova@gmail.com>
2017-04-23 22:03:00 -0700

    adjusted tests for orders to reflect that attributes are required only for orders that are not pending

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,15 @@ class Order < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :status, presence: true
-  validates :email, presence: true, format: {with: /@/}
-  validates :billing_name, presence: true
-  validates :address, presence: true
-  validates :card_number, presence: true, length: { in: 14..19 }
-  validates :card_expiration, presence: true
-  validates :cvv, presence: true, length: { in: 3..4 }
-  validates :zipcode, presence: true, length: { is: 5 }
+  validates :email, presence: true, format: {with: /@/}, unless: :in_cart?
+  validates :billing_name, presence: true, unless: :in_cart?
+  validates :address, presence: true, unless: :in_cart?
+  validates :card_number, presence: true, length: { in: 14..19 }, unless: :in_cart?
+  validates :card_expiration, presence: true, unless: :in_cart?
+  validates :cvv, presence: true, length: { in: 3..4 }, unless: :in_cart?
+  validates :zipcode, presence: true, length: { is: 5 }, unless: :in_cart?
+
+  def in_cart?
+    status == "pending"
+  end
 end
