8812e2f45b6a1618ccd7493bc33335ce3045cd87
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-23 21:13:05 -0700

    working on order#update/buy method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
   def cart
+    # raise
     @products = Productorder.where(order_id: session[:order_id])
   end
   # new
@@ -12,6 +13,7 @@ class OrdersController < ApplicationController
     end
 
     product_order = Productorder.add_product(params[:product_id], session[:order_id] )
+
     if product_order.valid?
       product_order.save
       redirect_to cart_path
@@ -28,10 +30,30 @@ class OrdersController < ApplicationController
 
   # buy method
   def update
-    validate_user_info(params)
+    @order = current_order
+    # @order.validate_user_info(params)
+    @order.update_attributes(order_params)
+
+    if @order.valid?
+      @order.status = "paid"
+      @order.save
+      session[:order_id] = nil
+      # raise
+      flash[:success] = "Thank you for placing your order"
+      redirect_to root_path
+    end
   end
 
 private
+  def order_params
+    return params.required(:order).permit(:customer_name,
+                                          :customer_email,
+                                          :customer_address,
+                                          :customer_city,
+                                          :customer_zipcode,
+                                          :customer_state,
+                                          :customer_cc_info)
+  end
 
 # strong params
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,17 @@ class Order < ApplicationRecord
   has_many :products, through: :productorders
   has_and_belongs_to_many :products
 
-  # validates :customer_name, presence: true
-  # validates :customer_address, presence: true
-  # validates :customer_email, presence: true
-  # validates :customer_cc_info, presence: true,
-                              #  numericality: { only_integer: true },
-                              #  length: { is: 16 }
-  # validates :status, inclusion: { in: %w(paid) }
+
+
+  # def self.validate_user_info(params)
+  #   self.customer_name =
+  #   validates :customer_name, presence: true
+  #   validates :customer_address, presence: true
+  #   validates :customer_email, presence: true
+  #   validates :customer_cc_info, presence: true,
+  #                                numericality: { only_integer: true },
+  #                                length: { is: 16 }
+  #   validates :status, inclusion: { in: %w(paid) }
+  # end
+
 end
