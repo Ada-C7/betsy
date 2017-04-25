dd178a7a5870bc38adebc377554b20ff9acd93ef
Spatterjaaay <aja.pursova@gmail.com>
2017-04-25 10:29:45 -0700

    wrote total_cost method in orders model, and subtotal method in order_item model and referred to them in the order confirmation page

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,9 @@ class OrderItem < ApplicationRecord
   belongs_to :order
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def subtotal(ordered_item)
+    subtotal = ordered_item.quantity * ordered_item.product.price
+    return "$ #{subtotal.round(2)}"
+  end
 end
