39cce7b8c80fa22f049d8dcd8f95979f6a253481
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-25 13:11:40 -0700

    adds basic sidebar style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :products
   has_many :orders
+  has_many :order_items, through: :products
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
@@ -15,12 +16,14 @@ class User < ApplicationRecord
   end
 
   def order_revenue(order)
-    order_items = OrderItem.where(product: products, order: order)
-    order_items.map { |order_item| order_item.subtotal }.inject(:+)
+    order_items.select { |order_item|
+      order_item.order == order
+    }.map { |order_item|
+      order_item.subtotal
+    }.inject(:+)
   end
 
   def total_revenue
-    order_items = OrderItem.where(product: products)
     order_items.map { |order_item| order_item.subtotal }.inject(:+)
   end
 
