f0c8677a8dd1add82c3b05f99ffbff84ce4526da
amb54 <ambarklund@hotmail.com>
2017-04-25 14:36:23 -0700

    Added resources for categories with product index nested.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   # get '/login', to: 'merchantss#login_form', as: 'login'
   # post '/login', to: 'merchants#login'
   # post '/logout', to: 'merchants#logout', as: 'logout'
+  resources :categories do
+    get '/products', to: 'products#index'
+    #resourses :products, only: [:index]    ----is another way to write this
+  end
+
 
   resources :products
   patch 'products/:id/status', to: 'products#status', as: 'status'
