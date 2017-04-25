710a5e5c6745b4a88e33a6240e0fc32dd5717b81
amb54 <ambarklund@hotmail.com>
2017-04-25 14:33:12 -0700

    Schema updated after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421163758) do
+ActiveRecord::Schema.define(version: 20170425171105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_categories_products_on_category_id", using: :btree
+    t.index ["product_id"], name: "index_categories_products_on_product_id", using: :btree
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.string   "username"
@@ -37,13 +50,13 @@ ActiveRecord::Schema.define(version: 20170421163758) do
   end
 
   create_table "productorders", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "order_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "products_id"
+    t.integer  "orders_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "quantity"
-    t.index ["order_id"], name: "index_productorders_on_order_id", using: :btree
-    t.index ["product_id"], name: "index_productorders_on_product_id", using: :btree
+    t.index ["orders_id"], name: "index_productorders_on_orders_id", using: :btree
+    t.index ["products_id"], name: "index_productorders_on_products_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
