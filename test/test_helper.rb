6801f55a2f809077bfe15e1f83730dd9970773be
amb54 <ambarklund@hotmail.com>
2017-04-26 15:45:50 -0700

    Added code to handle login at testing (copied from lectures)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,6 +26,29 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+
+  def setup
+    #Make OmniAuth accept mockdata in tests
+    OmniAuth.config.test_mode = true
+  end
+
+  def mock_auth_hash(user)
+    return {
+      provider: user.oauth_provider,
+      uid: user.oauth_uid,
+      info: {
+        email: user.email,
+        nickname: user.username
+      }
+    }
+  end
+
+  def login(user)
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get auth_callback_path
+  end
+
+
   def get_current_order
     post current_order
   end
