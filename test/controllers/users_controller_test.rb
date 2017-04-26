d5b2b26bea3ee1deccdef8c09fe2c2de39a91d6b
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-25 22:50:51 -0700

    moves require_login to users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "update" do
+    it "updates the user" do
+      patch account_path, params: { user: { username: "Bob", email: "bob@bob.com", image_url: "bob.jpg" } }
+      must_redirect_to root_path
+    end
+
+    it "returns bad_request and fails to update user when given invalid data" do
+      patch account_path, params: { user: { } }
+      must_respond_with :bad_request
+    end
+  end
 end
