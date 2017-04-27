4bd3f9ca231febc21703e1cab14ed823ea53d42f
Brenna <bcmdarroch@gmail.com>
2017-04-27 15:20:01 -0700

    divided controller tests btwn guests and logged in users

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,36 +3,41 @@ require "test_helper"
 describe CategoriesController do
   let(:category) { categories(:comfy) }
 
-  describe "new" do
-    it "runs successfully" do
-      skip
-      get new_category_path
-      must_respond_with :success
-    end
-  end
+  describe "logged in users" do
 
-  describe "create" do
-    it "creates a new category" do
-      post categories_path, params: { category: { name: 'cozy'} }
-      must_redirect_to root_path
+    describe "new" do
+      it "runs successfully" do
+        skip
+        get new_category_path
+        must_respond_with :success
+      end
     end
 
-    it "adds a new category to the database" do
-      proc {
+    describe "create" do
+      it "creates a new category" do
+        skip
         post categories_path, params: { category: { name: 'cozy'} }
-      }.must_change 'Category.count', 1
-    end
+        must_redirect_to root_path
+      end
 
-    it "returns 404 and fails to create a new category w invalid data" do
-      post categories_path, params: { category: { name: nil } }
-      must_respond_with :bad_request
-    end
+      it "adds a new category to the database" do
+        skip
+        proc {
+          post categories_path, params: { category: { name: 'cozy'} }
+        }.must_change 'Category.count', 1
+      end
 
-    it "does not add a new category to the database" do
-      proc {
+      it "returns 404 and fails to create a new category w invalid data" do
         post categories_path, params: { category: { name: nil } }
-      }.must_change 'Category.count', 0
-    end
-  end
+        must_respond_with :bad_request
+      end
 
+      it "does not add a new category to the database" do
+        proc {
+          post categories_path, params: { category: { name: nil } }
+        }.must_change 'Category.count', 0
+      end
+    end
+
+  end
 end
