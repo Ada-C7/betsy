e9bc2292e6728d5e715cd88d5f8a86c72257edfd
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-26 21:38:01 -0700

    moves oauth tests to sessions controller

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 describe SessionsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "auth_callback" do
+    it "logs in an existing user and redirects to the root" do
+      proc {
+        user = users(:one)
+        login(user)
+
+        must_redirect_to root_path
+        session[:user_id].must_equal user.id
+
+      }.wont_change 'User.count'
+    end
+
+    it "creates a new user and renders the account edit page" do
+      proc {
+        user = User.new(provider: "github", uid: 789, username: "Bob", email: "bob@bob.com", image_url: "bob.jpg")
+        login(user)
+
+        must_redirect_to edit_account_path
+      }.must_change 'User.count', 1
+
+      session[:user_id].must_equal User.last.id
+    end
+
+    it "does not create a new user if an invalid hash is received" do
+      proc {
+        user = User.new(provider: "github", uid: 999)
+        login(user)
+      }.wont_change 'User.count'
+
+      must_redirect_to root_path
+    end
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -16,30 +16,6 @@ describe UsersController do
     end
   end
 
-  describe "auth_callback" do
-    it "logs in an existing user and redirects to the root" do
-      proc {
-        user = users(:one)
-        login(user)
-
-        must_redirect_to root_path
-        session[:user_id].must_equal user.id
-
-      }.wont_change 'User.count'
-    end
-
-    it "creates a new user and renders the account edit page" do
-      proc {
-        user = User.new(provider: "github", uid: 789, username: "Bob", email: "bob@bob.com", image_url: "bob.jpg")
-        login(user)
-
-        must_redirect_to edit_account_path
-      }.must_change 'User.count', 1
-
-      session[:user_id].must_equal User.last.id
-    end
-  end
-
   describe "logged in users" do
     before do
       login(users(:one))
