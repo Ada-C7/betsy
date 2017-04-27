ee70b882187bbec31b7078291efe470b10637c20
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-27 13:54:27 -0700

    fixes require_login bug in products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :retire, :review]
-  skip_before_action :require_login,  only: [:new, :edit, :retire]
+  skip_before_action :require_login,  except: [:new, :edit, :retire]
 
   def homepage
     @products = Product.where(retired: false)
