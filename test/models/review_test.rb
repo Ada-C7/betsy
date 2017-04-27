434b5bcab480f2f0e525b32e9eb45f201b5c32c5
Brenna <bcmdarroch@gmail.com>
2017-04-27 13:12:34 -0700

    fixed reviews so product owner can't review their own product

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,6 +1,35 @@
 require "test_helper"
 
 describe Review do
-  let(:review) { Review.new }
+  let(:review) { reviews(:goodone, :badone).sample }
+
+  describe "relationships" do
+    it "belongs to a product" do
+
+    end
+  end
+
+  describe "validations" do
+    it "is valid with an integer rating 1-5" do
+
+    end
+
+    it "is invalid without a rating" do
+
+    end
+
+    it "is invalid without an integer rating" do
+
+    end
+
+    it "is invalid without a rating greater than 0" do
+
+    end
+
+    it "is invalid without a rating less than 6" do
+
+    end
+  end
+
 
 end
