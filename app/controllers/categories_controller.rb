5d7eacfd160ec8eb237ce33d9c95ef53d542c221
amb54 <ambarklund@hotmail.com>
2017-04-26 17:15:44 -0700

    Added the methods new and create and the private method category_params.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,29 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      # flash[:status] = :success
+      # flash[:result_text] = "Successfully created #{@product.id}"
+      redirect_to categories_path
+    else
+      # flash[:status] = :failure
+      # flash[:result_text] = "Could not create #{@product.id}"
+      # flash[:messages] = @product.errors.messages
+      render :new #, status: :bad_request
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+
 end
