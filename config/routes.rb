84e4899a8ef96057dfdbe768fac5013f73914334
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-28 10:22:57 -0700

    cleaned up the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get '/logout', to: "merchants#logout", as: 'logout'
 
   resources :products do
-    # post '/add_item', to: 'orders#add_item'
     get '/categories/new', to: 'products#new_category', as: 'new_category'
     post '/categories', to: 'products#create_category', as: 'categories'
     post '/add_product', to: 'orders#add_product'
@@ -14,16 +13,12 @@ Rails.application.routes.draw do
 
   resources :categories do
     get '/products', to: 'products#index'
-    #resourses :products, only: [:index]    ----is another way to write this
   end
 
   resources :reviews
 
   resources :merchants, only: [:index, :show]
   patch 'products/:id/status', to: 'products#status', as: 'status'
-
-  # get 'products/:id/new_category_product', to: 'products#new_category_product', as: 'add_category'
-  # post 'products/:id/', to: 'products#create_category_product'
 
   get '/cart', to: 'orders#cart', as: 'cart'
   get '/checkout', to: 'orders#checkout', as: 'checkout'
