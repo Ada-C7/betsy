4948894f33241c085e04c3673d2381f1f63fe366
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-28 12:07:13 -0700

    fixed merchant show view bug

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,32 +6,11 @@ class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find_by(id: params[:id])
     if @merchant.nil?
-      head :not_found
+      flash[:status] = :failure
+      flash[:result_text] = "Merchant Page Not Found"
+      redirect_to merchants_path
     end
   end
-
-  # def login_form;  end
-  #
-  # def login
-  #   merchant = Merchant.find_by(username: params[:username])
-  #   if user
-  #     session[:merchant_id] = merchant.id
-  #     flash[:success] = "Successfully logged in as existing user #{merchant.username}"
-  #     redirect_to root_path
-  #   elsif
-  #     merchant = Merchant.create(username: params[:username])
-  #     session[:merchant_id] = merchant.id
-  #     flash[:success] = "Successfully created new user #{merchant.username} with ID #{merchant.id}"
-  #     redirect_to root_path
-  #   else
-  #     if merchant.errors.any?
-  #       merchant.errors.each do |column, message|
-  #         flash.now[:error] = "Could not log in #{message}"
-  #       end
-  #       render :login_form
-  #     end
-  #   end
-  # end
 
   def auth_callback
     auth_hash = request.env['omniauth.auth']
